34eac0ad71bc84bfee87e0b6cfea98b67df1b73b
Kensy Soriano <kensysoriano22@gmail.com>
2024-10-08 23:17:30 -0600

    Portafolio

---
+++ index.js
@@ -0,0 +1,8 @@
+function redirigirEnlaceRepo() {
+    // Para redirigir al enlace
+    window.location.href = "https://github.com/ksorie/Encriptador-Alura";
+}
+function redirigirEnlaceDemo() {
+    // Para redirigir al enlace
+    window.location.href = "https://encriptador-alura-eight.vercel.app/";
+}
